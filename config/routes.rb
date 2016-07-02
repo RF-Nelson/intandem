54f5e903217e5515f12d06ceca4f6f9e620bd255
Richard Nelson <rnelson@alumni.upenn.edu>
2016-07-02 16:45:25 -0400

    add forums

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
         sessions: 'users/sessions'
       }
+
+  mount Thredded::Engine => '/forum'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
   root 'welcome#index'
 
   # Example of regular route:
