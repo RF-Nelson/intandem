3aff1c890d2d8d306c023718d65e619ac351341f
bryanmillstein <bryanmillstein@gmail.com>
2016-07-03 16:51:29 -0400

    Switches to ascending order.

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,7 +1,7 @@
 class BlogPostsController < ApplicationController
 
   def index
-    @blog_posts = BlogPost.all.order(created_at: :desc)
+    @blog_posts = BlogPost.all.order(created_at: :asc)
   end
 
 end
