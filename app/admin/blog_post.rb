4c91bf3202ad87abd988bd6698b914c871ceea5d
bryanmillstein <bryanmillstein@gmail.com>
2016-07-03 19:04:23 -0400

    Removes unnecessary fields.

--- app/admin/blog_post.rb
+++ app/admin/blog_post.rb
@@ -1,12 +1,10 @@
 ActiveAdmin.register BlogPost do
-  permit_params :title, :body, :image_url, :image_description
+  permit_params :title, :body
 
   form do |f|
     f.inputs do
       f.input :title
       f.input :body, as: :html_editor
-      f.input :image_url
-      f.input :image_description
     end
 
     f.actions
