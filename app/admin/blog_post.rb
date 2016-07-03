2eb168208e4b65677b106407d628323077319ab7
bryanmillstein <bryanmillstein@gmail.com>
2016-07-03 18:40:18 -0400

    Adds html editor to blog posts.

--- app/admin/blog_post.rb
+++ app/admin/blog_post.rb
@@ -1,17 +1,16 @@
 ActiveAdmin.register BlogPost do
+  permit_params :title, :body, :image_url, :image_description
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-permit_params :title, :body, :image_url, :image_description
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :body, as: :html_editor
+      f.input :image_url
+      f.input :image_description
+    end
+
+    f.actions
+  end
 
 
 end
